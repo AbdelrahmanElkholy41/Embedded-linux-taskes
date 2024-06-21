8b2135b29d52d0b6906579c933bd2412c8a81103
Abdelrahman Elkhoily <abdelrahmanelkholy98@gmail.com>
2024-06-22 01:40:25 +0300

    Delete week1 directory

--- week1/task3.py
+++
@@ -1,9 +0,0 @@
-import os
-print(os.environ)
-print('-------------------------------')
-print()
-print(os.environ['HOME'])
-print('-------------------------------')
-print()
-print(os.environ['PATH'])
-print('-------------------------------')
