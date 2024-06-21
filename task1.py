77c4377bcd3410f443f1c279a410a010f6d3aa08
Abdelrahman Elkhoily <abdelrahmanelkholy98@gmail.com>
2024-06-21 20:29:41 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+my_list=[4,5,6,3,2,8,4,1,2,9,6,4,8,49,8,1,4,4,4]
+countt=0
+for i in my_list:
+          if i == 4:
+                  countt+=1     
+print(countt)
+print('-----------------------------')
+my_list=[4,5,6,3,2,8,4,1,2,9,6,4,8,49,8,1,4,4,4]
+count_0=my_list.count(4)
+print(count_0)
+print()
+print('------------------------------')
