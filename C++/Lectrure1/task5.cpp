57e47bca78bd770db49d26219e51be5740df63cf
Abdelrahman Elkhoily <abdelrahmanelkholy98@gmail.com>
2024-07-28 20:18:56 +0300

    Create task5.cpp

---
+++ C++/Lectrure1/task5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+int main()
+{
+
+    for (int  i = 1; i <=12; i++)
+    {
+        std::cout<<"multiplication " << i<<std  ::  endl;
+        for (int j= 1; j <=12; j++)
+        {
+            
+        std::cout <<i<<" * "<<j<<" = "<<i*j << std::endl;
+        
+    }
+    
+}
+}
