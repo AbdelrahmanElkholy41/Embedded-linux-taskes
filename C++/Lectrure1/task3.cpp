9e5cd77a1d9771ab988ab4dc29a3954dad9c603b
Abdelrahman Elkhoily <abdelrahmanelkholy98@gmail.com>
2024-07-28 19:28:58 +0300

    Create task3.cpp

---
+++ C++/Lectrure1/task3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+  for(int i=1;i<=10;i++){  
+        for(int j=1;j<=i;j++)
+        {
+            std::cout<<"*";
+            
+        }
+        std::cout<<std::endl;
+      
+    }
+}
