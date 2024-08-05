3b7fafe29545d7286a57e35e004454749c589fb1
Abdelrahman Elkhoily <abdelrahmanelkholy98@gmail.com>
2024-08-05 19:56:46 +0300

    Create check if the character is digit.cpp

---
+++ C++/Lecture5/check if the character is digit.cpp
@@ -0,0 +1,25 @@
+#include <bitset>
+#include <iostream>
+#include <algorithm>
+#include <regex>
+#include <string>
+#include <vector>
+
+
+
+int main() {
+    char character = '5';
+
+    if (isdigit(character)) {
+        std::cout << character << " is a digit." << std::endl;
+    } else {
+        std::cout << character << " is not a digit." << std::endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
