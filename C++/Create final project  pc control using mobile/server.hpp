11b87d5c266b76a1686876f20e8673ebcaefaad5
Abdelrahman Elkhoily <abdelrahmanelkholy98@gmail.com>
2024-09-06 17:36:00 +0300

    Update server.hpp

--- C++/Create final project  pc control using mobile/server.hpp
+++ C++/Create final project  pc control using mobile/server.hpp
@@ -1,1 +1,31 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
 
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <array>
+
+class ServerController {
+    private:
+        int socket_id;
+        int client_id;
+        std::array<char, 50000> buffer;
+
+    public:
+        ServerController();
+        ~ServerController();
+        bool socket_init();
+        bool server_listen();
+        bool server_accept_client();
+        bool server_send(const std::string &message);
+        std::string server_read();
+        void close_listeing();
+        void close_listeing_client();
+        void close_server();
+
+};
+
+#endif
